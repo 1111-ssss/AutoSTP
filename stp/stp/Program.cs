41855a57227bcbbcd8f33e9c04ba180bdb2f0c4b
Виктор <kasa.king_over@mail.ru>
2025-11-24 13:52:18 +0300

    doxc

--- stp/stp/Program.cs
+++ stp/stp/Program.cs
@@ -2,15 +2,20 @@
 using System.Runtime.InteropServices;
 using System.IO;
 using DocXFunc;
-
+using System.Globalization;
+using openXMlFunc;
 
 class Program
 {
     static void Main()
     {
-        // 1. Запускаем Word (в фоне)
-        DoXcConveer doXcConveer = new DoXcConveer(@"C:\Users\Rog\Desktop\ТПО ПР1.docx");
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+        CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+
+        DoXcConveer doXcConveer = new DoXcConveer(@"C:\Users\Rog\Desktop\Отчет 19 Гурский.docx");
         doXcConveer.AllConveer();
-        doXcConveer.Save(@"C:\Users\Rog\Desktop\ТПО ЛР22 Касевич В.А..docx");
+        doXcConveer.Save();
+        class1.ReplaceStylesWithFourCustom(@"C:\Users\Rog\Desktop\Отчет 19 Гурский.docx");
     }
 }
