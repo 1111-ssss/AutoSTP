06088d901dcc7c9cba30675d6c1252ed6d4d00d7
1111-ssss <gurskievgeni@gmail.com>
2025-11-25 10:14:28 +0300

    fix

--- stp/openXMlFunc/Style/Metatags/Metatags.cs
+++ stp/openXMlFunc/Style/Metatags/Metatags.cs
@@ -4,12 +4,13 @@ namespace openXMlFunc.Metatags
 {
     class Metatags
     {
-        public static void AddMetatags(WordprocessingDocument doc, String author = "", String title = "", String subject = "", String description = "")
+        public static void AddMetatags(WordprocessingDocument doc, String author = "", String title = "", String subject = "", String description = "", String lastModifiedBy = "")
         {
             doc.PackageProperties.Creator = author;
             doc.PackageProperties.Title = title;
             doc.PackageProperties.Subject = subject;
             doc.PackageProperties.Description = description;
+            doc.PackageProperties.LastModifiedBy = lastModifiedBy;
         }
     }
 }
